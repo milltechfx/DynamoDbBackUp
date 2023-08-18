cd413920f5d1d41cb303dee857de14173bdd6720
Joe Vaughan <joseph-vaughan@hotmail.co.uk>
2023-08-18 13:01:46 +0100

    fix: try await restore

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const Restore = require("./lib/restore");
 const Backup = require("./lib/backup");
 const Deploy = require("./lib/deploy");
 
+const fullStackName = "dev-joeTest-stack-euw1";
+const datastore_name = "qa-limits-dynamoDb-euw1";
+const timestamp = "dd-MM-yyyy-HH-mm-ss";
+
+const dynamoConfig = {
+  S3Bucket: "qa-copydowndata-s3-euw1" /* required */,
+  S3Prefix: `backup/qa-LimitsDb-stack-euw1/DYNAMO/qa-limits-dynamoDb-euw1_DYNAMO_18-08-2023-11-22-16.json` /* optional */,
+  S3Region: "eu-west-1" /* required */,
+  DbTable: datastore_name /* required */,
+  DbRegion: "eu-west-1" /* required */,
+  mergedFiles: true,
+};
+
+Restore(dynamoConfig);
+//new Backup(dynamoConfig).full();
+
 module.exports = {
   Backup: Backup,
   Restore: Restore,
